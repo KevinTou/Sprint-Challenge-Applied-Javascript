b85fecdfd93293efe82e6f0e5d9df8642cc20900
Kevin Tou <toukevin@gmail.com>
2019-06-21 09:13:22 -0700

    Updated carousel toggle for stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,56 @@
 class Carousel {
+  constructor(props) {
+    this.props = props;
+    // console.log("this.props: ", this.props);
 
+    this.images = props.querySelectorAll("img");
+    // console.log("this.images: ", this.images);
+
+    this.leftButton = props.querySelector(".left-button");
+    // console.log("this.leftButton: ", this.leftButton);
+
+    this.rightButton = props.querySelector(".right-button");
+    // console.log("this.rightButton: ", this.rightButton);
+
+    this.currentIndex = 0;
+
+    this.images[0].style.display = "block";
+
+    this.leftButton.addEventListener("click", () => {
+      this.rotateLeft();
+    });
+
+    this.rightButton.addEventListener("click", () => {
+      this.rotateRight();
+    });
+  }
+
+  rotateLeft() {
+    this.images[this.currentIndex].style.display = "none";
+    if (this.currentIndex === 0) {
+      this.currentIndex = this.images.length - 1;
+      this.images[this.currentIndex].style.display = "block";
+    } else {
+      this.currentIndex -= 1;
+      this.images[this.currentIndex].style.display = "block";
+    }
+  }
+
+  rotateRight() {
+    this.images[this.currentIndex].style.display = "none";
+    if (this.currentIndex === this.images.length - 1) {
+      this.currentIndex = 0;
+      this.images[this.currentIndex].style.display = "block";
+    } else {
+      this.currentIndex += 1;
+      this.images[this.currentIndex].style.display = "block";
+    }
+  }
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector(".carousel");
+// console.log("carousel", carousel);
+new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
